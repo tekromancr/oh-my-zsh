e46cac150998ddc1531e7b9d5f0804c3bd8fae68
George "༼ つ ◕_◕ ༽つ" Griffin <george@griff.in>
2015-01-30 10:30:28 -0500

    Adds some aliases

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,7 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+alias activate='source ~/Code/venvs/verificient/bin/activate'
+alias runserver='python manage.py runserver 0.0.0.0:8000'
+alias prettybg="google-chrome --app='http://8bitdash.com/'"
+
