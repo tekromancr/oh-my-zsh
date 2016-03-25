1d0b078bd6e343cce8297d3af61c36ddc7151165
George "༼ つ ◕_◕ ༽つ" Griffin <george@griff.in>
2016-03-25 10:56:00 -0400

    commits my personal aliases

---
+++ custom/custom.zsh
@@ -0,0 +1,14 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+alias verificient='source ~/Code/venvs/verificient/bin/activate'
+alias runserver='python manage.py runserver 0.0.0.0:8000'
+alias prettybg="google-chrome --app='http://8bitdash.com/'"
+alias notes='vi /home/george/Notes/"`date +\"%Y-%m-%d\"`".txt'
+alias vnotes='ls /home/george/Notes/'
+alias tmux='tmux -2'
+alias weather='finger new_york@graph.no'
+c2f() {echo "$(( ($1*(9.0/5.0))+32 ))"}
+f2c() {echo "$(( ($1-32)*(5.0/9.0)  ))"}
